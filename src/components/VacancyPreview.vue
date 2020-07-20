006836e7902f7bc99a4c55835e1f888507481323
Maxim Bredikhin <max11298@yandex.ru>
2020-07-21 00:11:12 +0300

    Create vacancy card

---
+++ src/components/VacancyPreview.vue
@@ -0,0 +1,151 @@
+<template>
+  <div class="card" @click="$emit('onClick', id)">
+    <div class="image">
+      <img class="card__image" :src="company_logo" :alt="company" />
+    </div>
+    <div class="content">
+      <span class="card__company">{{ company }}</span>
+      <span class="card__title">{{ title }}</span>
+      <span v-if="isFullTime" class="card__badge">Full time</span>
+      <div class="footer">
+        <div class="card__extra card__extra--location">
+          <img class="card__icon" :src="require('@/assets/clock.svg')" />
+          <span class="card__extra-text">{{ location }}</span>
+        </div>
+        <div class="card__extra card__extra--created">
+          <img class="card__icon" :src="require('@/assets/world.svg')" />
+          <span class="card__extra-text">{{ created_at }}</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    company: {
+      type: String,
+      required: true,
+    },
+
+    company_logo: {
+      type: String,
+      required: true,
+    },
+
+    created_at: {
+      type: String,
+      required: true,
+    },
+
+    isFullTime: {
+      type: Boolean,
+      default: true,
+    },
+
+    location: {
+      type: String,
+      required: true,
+    },
+
+    title: {
+      type: String,
+      required: true,
+    },
+
+    id: {
+      type: String,
+      required: true,
+    },
+  },
+
+  computed: {
+    created() {
+      return new Date() - this.created_at;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.card {
+  display: flex;
+  min-height: 8rem;
+  padding: 0.5rem;
+  background: white;
+  border-radius: 4px;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.05);
+}
+
+.image {
+  width: 25%;
+  padding: 0.25rem;
+}
+
+.card__image {
+  display: inline-block;
+  width: 100%;
+}
+
+.content {
+  padding-left: 0.5rem;
+  width: 75%;
+  display: flex;
+  flex-flow: column nowrap;
+  font-family: 'Roboto', sans-serif;
+  justify-content: space-between;
+}
+
+.card__company {
+  font-weight: bold;
+  font-size: 12px;
+  line-height: 14px;
+  color: #334680;
+  padding: 0.25rem 0;
+}
+
+.card__title {
+  font-size: 16px;
+  line-height: 19px;
+  color: #334680;
+  padding: 0.25rem 0;
+}
+
+.card__badge {
+  display: inline-block;
+  text-align: center;
+  max-width: 4rem;
+  padding: 0.2rem;
+  font-size: 12px;
+  color: #334680;
+  font-weight: 500;
+  border: 1px solid #334680;
+  border-radius: 4px;
+  margin: 0.5rem 0;
+}
+
+.card__location {
+  font-size: 12px;
+  color: #b9bdcf;
+  border-radius: 4px;
+  padding: 0.25rem 0;
+}
+
+.card__extra {
+  display: inline-flex;
+  margin-right: 0.75rem;
+  font-size: 12px;
+  color: #b7bcce;
+  padding: 0.25rem 0;
+}
+
+.card__extra-text {
+  line-height: 2;
+}
+
+.card__icon {
+  width: 18px;
+  margin-right: 0.25rem;
+}
+</style>
